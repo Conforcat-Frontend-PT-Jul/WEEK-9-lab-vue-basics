6c8ff43d4f1884e4b728a2d9827d21f9301e2b3b
Arturo Rubio <arubio@seocom.agency>
2022-09-20 22:08:07 +0200

    done

---
+++ src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  <nav>
+    <ul>
+      <li><a href="#">Item 1</a></li>
+      <li><a href="#">Item 2</a></li>
+      <li><a href="#">Item 3</a></li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+  nav {
+    margin-bottom: 30px;
+    color: hsla(160, 100%, 37%, 1);
+  }
+
+  ul {
+    list-style: none;
+    display: flex;
+    justify-content: center;
+  }
+
+  li {
+    padding: 0 20px;
+  }
+</style>
---
+++ src/components/iterations/Iteration2.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  data() {
+    return {
+      sumOperation2: 2 + 2
+    }
+  },
+  methods: {
+    sumItems(a, b) {
+      return a + b
+    }
+  },
+  mounted() {
+    this.$el.textContent += 'aaaa'
+  }
+}
+</script>
+
+<template>
+  <div class="sums">
+    <p>Sum 1: {{ 2 + 2 }} / </p>
+    <p>Sum 2: {{ sumOperation2 }} / </p>
+    <p>Sum 3: {{ sumItems(2, 2) }} / </p>
+  </div>
+</template>
+
+<style>
+.sums {
+  border: 1px solid grey;
+  padding: 30px;
+}
+</style>
