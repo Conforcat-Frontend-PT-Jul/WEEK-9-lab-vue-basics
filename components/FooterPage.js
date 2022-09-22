e875dc1283cb0407930fbfb4f17af04afe4b6503
Leandro Balaguer <leandro.balaguer@gmail.com>
2022-09-23 01:42:11 +0200

    done

---
+++ components/FooterPage.js
@@ -0,0 +1,19 @@
+app.component('footer-page', {
+    data(){
+        return{
+            footer:{}
+        }
+    },
+    template:
+    /*html*/
+   `
+   <div class="footer-page">
+    <ul>
+      <li>Manifesto</li>
+      <li>Work with Us</li>
+      <li>Privacy Policy</li>
+    </ul>
+    
+    </div>
+    `
+  });
---
+++ components/NavbarPage.js
@@ -0,0 +1,19 @@
+app.component('navbar-page', {
+    data(){
+        return{
+            menu:{}
+        }
+    },
+    template:
+    /*html*/
+   `
+   <div class="nav-bar">
+    <ul>
+      <li>Home</li>
+      <li>AboutUs</li>
+      <li>Contact!</li>
+    </ul>
+    </div>
+    `
+  });
+
