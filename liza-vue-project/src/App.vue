fa9374a9c897b94d6ae45aba9bfd3781b464c8f2
Mihhedkol <mihhdekol@gmail.com>
2022-09-22 21:38:04 +0200

    done

---
+++ liza-vue-project/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Navbar from './components/Navbar.vue'
+import Section from './components/Section.vue'
+import Footer from './components/Footer.vue'
+
+</script>
+
+<template>
+  <div id="main">
+    <Navbar />
+    <Section />
+    <Footer />
+  </div>
+</template>
+
+<style>
+
+#main {
+  margin: 0 auto;
+  padding: 2rem;
+  font-weight: normal;
+}
+
+
+</style>
---
+++ liza-vue-project/src/components/Footer.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <footer>Footer</footer>
+</template>
+
+<style scoped>
+  footer {
+    padding: 1rem 1rem; 
+    background: #cfd8dc;
+  }
+</style>
