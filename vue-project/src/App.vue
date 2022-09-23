cdda8e5767aaaa0d78603ae68c0956aca9347498
claudiamurall <claudiamurall@gmail.com>
2022-09-23 15:06:08 +0200

    iteration 1

---
+++ vue-project/src/App.vue
@@ -0,0 +1,31 @@
+<script>
+import TheNavbar from "./components/navbar.vue";
+import TheFooter from "./components/footer.vue";
+
+export default {
+  name: "App",
+  components: {
+    TheNavbar,
+    TheFooter,
+  },
+};
+</script>
+
+<template>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
+    <div class="wrapper">
+      <TheNavbar />
+    </div>
+  </header>
+  <main></main>
+  <TheFooter />
+</template>
+
+<style scoped></style>
---
+++ vue-project/src/components/TheFooter.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+  name: "TheFooter",
+};
+</script>
+
+<template><h1>adéu</h1></template>
+
+<style scoped></style>
