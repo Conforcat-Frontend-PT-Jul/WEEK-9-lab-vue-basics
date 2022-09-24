7a5817d43ad04efd58304cf6e299cf4383c79c87
Marga <margarita.benitez.lopez@gmail.com>
2022-09-24 16:52:34 +0200

    [BCN-PT-072022] Marga Benítez

---
+++ vue-project/src/components/Footer-bar.vue
@@ -0,0 +1,4 @@
+<script setup></script>
+<template>
+  <p>Napple Inc. One Napple Park Way; Cupertino, CA 95014, U.S.A.</p>
+</template>
---
+++ vue-project/src/components/Nav-bar.vue
@@ -0,0 +1,26 @@
+<!-- <script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script> -->
+<script setup></script>
+<template>
+  <ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">About Us</a></li>
+    <li><a href="#">Contact Us</a></li>
+  </ul>
+</template>
+
+<style scoped>
+ul {
+  display: flex;
+  justify-content: space-evenly;
+
+  list-style: none;
+}
+</style>
