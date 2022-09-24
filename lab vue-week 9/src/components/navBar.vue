df1fd927f2ebd53989268d05376fbbdadf0d7960
Viviana Velez Montoya <vivianavelezm@hotmail.com>
2022-09-24 20:21:38 +0200

    done

---
+++ lab vue-week 9/src/components/navBar.vue
@@ -0,0 +1,57 @@
+
+<template>
+
+    <nav>
+        <ul>
+            <li>
+                <a href="#Home">Home</a>
+            </li>
+            <li>
+                <a href="/project-page.html">Project</a>
+            </li>
+            <li>
+                <a href="#services">Services</a>
+            </li>
+        </ul>
+    </nav>
+
+  </template>
+
+<style scoped>
+    nav{
+    display:flex;
+    justify-content: space-around;
+    flex-direction: row;
+    margin: 10px;
+    align-items: center;
+    }
+
+    nav ul{
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    }
+
+    nav ul li a{
+    text-decoration: none;
+    color: #072AC8;
+    padding: 10px 30px;
+    font-size: 15px;
+    font-weight: bold;
+    border-radius: 10px;
+    }
+
+    nav ul li a:hover{
+    text-decoration: none;
+    color: #072AC8;
+    border: 1px solid #A2D6F9;
+    /*color: var(--hospital-green);*/
+    height: 30px;
+    }
+</style>
+
+
+        
---
+++ lab vue-week 9/src/components/theSection.vue
@@ -0,0 +1,116 @@
+<script>
+export default {
+    data(){
+        return{
+            suma: 2 + 2,
+            message: "",
+            hidden: false,
+            recipes:[
+                {
+                    name: 'Recipe 1',
+                    ingredients: 'Ingredients 1',
+                    time:'20 min',  
+                },
+                {
+                    name: 'Recipe 2',
+                    ingredients: 'Ingredients 2',
+                    time:'30 min',  
+                },
+                {
+                    name: 'Recipe 3',
+                    ingredients: 'Ingredients 3',
+                    time:'40 min',  
+                },
+            ]
+        };
+    },
+
+
+    methods:{
+      
+    }, 
+
+    mounted(){
+    this.message = 'The result is:';
+    },
+
+}
+</script>
+
+<template>
+<p> {{ suma }} </p>
+<p> {{ message }}</p>
+<p>2+2 = 
+    <span v-show="hidden">{{suma}}</span>
+</p>
+<button @:click="hidden = true"> Show </button>
+
+    <table>
+       <thead>
+            <tr>
+                <th>Recipe</th>
+                <th>Ingredients</th>
+                <th>Time</th>
+             </tr>   
+        </thead>
+        <tbody>
+            <tr v-for="recipe in recipes">
+                <td>{{ recipe.name }}</td>
+                <td>{{ recipe.ingredients}}</td>
+                <td>{{ recipe.time }}</td>
+            </tr>
+        </tbody>
+    </table>
+
+</template>
+
+<style scoped>
+
+p{
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-size: 15px;
+    color: blue;
+    margin: 30px;
+}
+
+span {
+  font-weight: bold;
+  color: blueviolet;
+  font-size: 15px;
+}
+
+button {
+  border: none;
+  background-color:blue;
+  color: white;
+  padding: 20px 30px;
+  margin: 30px;
+  font-size: 15px;
+  font-weight: bold;
+  border-radius: 50px;
+}
+button:hover {
+  cursor: pointer;
+  background-color: blueviolet;
+}
+
+table{
+    width: 100%;
+    margin: 30px;
+}
+
+table thead tr th{
+    font-weight: bold;
+    background-color: blueviolet;
+    color: antiquewhite;
+}
+
+td,th { 
+    width: 25%;
+    border-collapse: collapse;
+    border: 1px solid blueviolet; 
+    text-align: center;
+
+}
+   
+</style>
