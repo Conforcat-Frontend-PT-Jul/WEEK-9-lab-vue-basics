71dd02917536facbe118024c7b2af53c968e6c10
laggart <laggart@gmail.com>
2022-09-26 13:03:48 +0200

    done

---
+++ vue-basics-lab/src/components/TheSum.vue
@@ -0,0 +1,17 @@
+<script setup>
+   const theSum = '2+2 = ';         
+</script>
+
+
+
+
+
+<template> 
+    <h2>{{ theSum }} {{ 2 + 2 }}</h2>
+    <div>
+        {{ timeBomb }}
+    </div>
+</template>
+<style scoped lang="scss">
+</style>
+
